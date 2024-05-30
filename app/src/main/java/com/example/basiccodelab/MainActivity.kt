b7b1ba4eaf5ed82f9c20b61cd7a68eaefd9f1a44
ramo2208 <ramo@sbb.rs>
2024-05-30 23:14:56 +0200

    starter-code

--- app/src/main/java/com/example/basiccodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiccodelab/MainActivity.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.example.basiccodelab.ui.theme.BasicCodeLabTheme
 
@@ -34,6 +40,24 @@ class MainActivity : ComponentActivity() {
 		}
 	}
 
+	@Composable
+	fun OnBoardScreen(modifier: Modifier = Modifier) {
+		var shouldShowOnboarding  by remember { mutableStateOf(true) }
+
+		Column (
+			modifier = Modifier.fillMaxSize(),
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Text("Welcome to the basic code lab")
+			Button(
+				modifier = Modifier.padding(vertical = 24.dp),
+				onClick = { shouldShowOnboarding = false }
+			) {
+				Text("Continue")
+			}
+		}
+	}
 
 	@Composable
 	fun MyApp(
@@ -70,6 +94,14 @@ class MainActivity : ComponentActivity() {
 					Text(if (expanded.value) "Show less" else "Show more")
 				}
 			}
+		}
+	}
+
+	@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+	@Composable
+	fun OnBoardScreenPreview(){
+		BasicCodeLabTheme {
+			OnBoardScreen()
 		}
 	}
 
